519593a916e8bf515c2dfd07fbb2ae1f6ad9a857
Alexander Roe <ajdezigns@gmail.com>
2016-10-06 14:59:02 -0400

    test

--- bash/lab2/gitupdate.sh
+++ bash/lab2/gitupdate.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
-#
-#
+
+#Notes START
+
+#This script updates my github repo and add a update message.
+
+#Notes STOP
+
+#Varables START
+
+message="$1"
+
+#Varables STOP
+
 git add -A
-read -p "Message for update:" message
+
+if [[ $message = *[!\ ]* ]]; then
+  git commit -m "$message"
+else
+  read -p "Message for update:" message
 git commit -m "$message"
+fi 
+
 git push
+
+
+
+
+

b4a05bf67e63aec3adef66822d7a92ded64afb33
Alexander Roe <ajdezigns@gmail.com>
2016-10-20 15:26:27 -0400

    updated lab4

--- bash/lab3/pickanumber.sh
+++ bash/lab3/pickanumber.sh
@@ -1,36 +1,35 @@
 #!/bin/bash
+# this script will ask the user for a random number
+#    between one and ten and will keep asking
+#    until they get it right
 
-#This program generates a random number from 1 to 10 then will ask for user
-#input untill they guess correctly 
+# Variables
+###########
+# we need a random number for them to guess between 1 and 10
+myNumber=$(($RANDOM % 10 + 1))
 
-#Varables START
+# since our loop is controlled by their guess, start with an incorrect guess
+guess=0
 
-randomnumber=$((1+ $RANDOM %10))
+# Main
+######
 
-#Varalbes STOP
-
-while true; do
-
-  echo -n "Guess a number from 1 to 10: "; read userguess;
-
-  if [ -n "$userguess" ]; then
-        if [ $userguess -eq $randomnumber ]; then
-           echo "Hurrah! You guessed correctly. it was $randomnumber."
-           echo ""
-           break
-           
-        elif [ "$userguess" = "q" ]; then
-           echo "bye."
-           echo ""
-        break
+# we aren't done until the user guesses our number, so keep asking until they get it right
+while [ $guess -ne $myNumber ]; do
+    # get a number from them
+    read -p "Pick a number from 1 to 10: " guess
     
-        else
-           echo "Sorry you guessed wrong try again."
-           echo ""
-        fi
-     
-  else
-    echo "You didn't give me any input"
-  fi
-
+    # ask them over and over if they didn't give us something that looks like it might be valid    while [[ ! "$guess" =~ ^[1-9]0*$ ]]; do
+        read -p "Pick a number from 1 to 10: " guess
+    done
+    
+    # give them feedback about their guess if it was wrong
+    if [ $guess -lt $myNumber ]; then
+        echo "Too small, try again"
+    elif [ $guess -gt $myNumber ]; then
+        echo "Too big, try again"
+    fi
 done
+
+# loop ended, so they got it right, let them know
+echo "You got it!"
